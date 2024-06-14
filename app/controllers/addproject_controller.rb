0a6f880d602934afdc1f3314373115625ff9d0db
Manny G <manny2144@icloud.com>
2024-06-14 14:18:17 -0400

    Back-End completed and Added AddProject page

---
+++ app/controllers/addproject_controller.rb
@@ -0,0 +1,4 @@
+class AddprojectController < ApplicationController
+    def index
+    end
+end
---
+++ test/controllers/addproject_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class AddprojectControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
